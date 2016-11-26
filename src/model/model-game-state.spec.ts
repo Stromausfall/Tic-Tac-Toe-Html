cbb0fe5f439d28310367d7f799d8ec06213b41ec
Matthias Auer <mail@matthias-auer.net>
2016-11-26 20:50:56 +0100

    implemented all winning conditions

--- src/model/model-game-state.spec.ts
+++ src/model/model-game-state.spec.ts
@@ -4,6 +4,7 @@ import * as sinon from 'sinon';
 import * as chai from 'chai';
 import {Model} from './model';
 import {GameState} from './game-state';
+import {TileState} from './tile/tile-state';
 
  
 @suite class ModelGameStateSpec {
@@ -12,5 +13,87 @@ import {GameState} from './game-state';
         model.initialize();
 
         chai.expect(model.getGameState()).equals(GameState.ONGOING);
+     }
+
+     @test "test that all horizontal wins are detected - and the observer is notified"() {
+         for (var y=0; y<3; y++) {
+            var model:Model = new Model();
+            var gameStateChanged:boolean = false;
+            model.initialize();
+            model.addObserver(() => { gameStateChanged = true; })
+
+            // click a whole row
+            for (var x=0; x<3; x++) {
+                chai.expect(gameStateChanged).equals(false);
+                chai.expect(model.getGameState()).equals(GameState.ONGOING);
+
+                model.getTile(x, y).setState(TileState.Circle);
+            }
+
+            chai.expect(gameStateChanged).equals(true);
+            chai.expect(model.getGameState()).equals(GameState.WON_BY_CIRCLE);
+         }
+     }
+
+     @test "test that all vertical wins are detected - and the observer is notified"() {
+         for (var x=0; x<3; x++) {
+            var model:Model = new Model();
+            var gameStateChanged:boolean = false;
+            model.initialize();
+            model.addObserver(() => { gameStateChanged = true; })
+
+            // click a whole row
+            for (var y=0; y<3; y++) {
+                chai.expect(gameStateChanged).equals(false);
+                chai.expect(model.getGameState()).equals(GameState.ONGOING);
+                
+                model.getTile(x, y).setState(TileState.Circle);
+            }
+
+            chai.expect(gameStateChanged).equals(true);
+            chai.expect(model.getGameState()).equals(GameState.WON_BY_CIRCLE);
+         }
+     }
+
+     @test "test that all diagonal wins are detected - and the observer is notified"() {
+         for (var reverse of [0,2]) {
+            var model:Model = new Model();
+            var gameStateChanged:boolean = false;
+            model.initialize();
+            model.addObserver(() => { gameStateChanged = true; })
+
+            // click a whole row
+            model.getTile(0, Math.abs(reverse - 0)).setState(TileState.Circle);
+            model.getTile(1, Math.abs(reverse - 1)).setState(TileState.Circle);
+            model.getTile(2, Math.abs(reverse - 2)).setState(TileState.Circle);
+
+            chai.expect(gameStateChanged).equals(true);
+            chai.expect(model.getGameState()).equals(GameState.WON_BY_CIRCLE);
+         }
+     }
+
+     @test "test that it is detected if no one wins - and the observer is notified"() {
+         for (var reverse of [0,2]) {
+            var model:Model = new Model();
+            var gameStateChanged:boolean = false;
+            model.initialize();
+            model.addObserver(() => { gameStateChanged = true; })
+
+            // click a whole row
+            model.getTile(0, 0).setState(TileState.Circle);
+            model.getTile(1, 0).setState(TileState.Circle);
+            model.getTile(2, 0).setState(TileState.Cross);
+            model.getTile(0, 1).setState(TileState.Cross);
+            model.getTile(1, 1).setState(TileState.Circle);
+            model.getTile(2, 1).setState(TileState.Circle);
+            model.getTile(0, 2).setState(TileState.Circle);
+            model.getTile(1, 2).setState(TileState.Cross);
+            
+            chai.expect(model.getGameState()).equals(GameState.ONGOING);
+            model.getTile(2, 2).setState(TileState.Cross);
+            
+            chai.expect(gameStateChanged).equals(true);
+            chai.expect(model.getGameState()).equals(GameState.WON_BY_NOONE);
+         }
      }
 }
--- src/model/model.ts
+++ src/model/model.ts
@@ -3,8 +3,10 @@ import {ModelView} from './model-view';
 import {ModelController} from './model-controller';
 import {Tile} from './tile/tile';
 import {GameState} from './game-state';
+import {TileState} from './tile/tile-state';
+import {Observable} from '../utils/observable';
 
-export class Model implements ModelView, ModelController {
+export class Model extends Observable implements ModelView, ModelController {
     private _tiles:Collections.Dictionary<String, Tile> = null;
     private _gameState:GameState = GameState.ONGOING;
 
@@ -15,14 +17,104 @@ export class Model implements ModelView, ModelController {
     initialize() : void {
         this._tiles = new Collections.Dictionary<String, Tile>();
 
+        // create tiles
         for (var x:number=0; x<3; x++) {
             for (var y:number=0; y<3; y++) {
                 var key:string = this.getTileKey(x, y);
                 var tile:Tile = new Tile(x, y);
 
+                // if the state of a tile changes - check the game state
+                tile.addObserver(() => { this.checkGameState() });
+
                 this._tiles.setValue(key, tile);
             }
         }
+    }
+
+    private checkForWin(tile1:Tile, tile2:Tile, tile3:Tile) {
+        if ((tile1.getState() == tile2.getState()) && (tile2.getState() == tile3.getState())) {
+            if (tile1.getState() == TileState.Circle) {
+                this._gameState = GameState.WON_BY_CIRCLE;
+                this.notify();
+            }
+
+            if (tile1.getState() == TileState.Cross) {
+                this._gameState = GameState.WON_BY_CROSS;
+                this.notify();
+            }
+        }
+    }
+
+    private checkHorizontal():void {
+        for (var y=0; y<3; y++) {
+            var tile1:Tile = this.getTile(0, y);
+            var tile2:Tile = this.getTile(1, y);
+            var tile3:Tile = this.getTile(2, y);
+
+            this.checkForWin(tile1, tile2, tile3);
+        }
+    }
+
+    private checkVertical():void {
+        for (var x=0; x<3; x++) {
+            var tile1:Tile = this.getTile(x, 0);
+            var tile2:Tile = this.getTile(x, 1);
+            var tile3:Tile = this.getTile(x, 2);
+
+            this.checkForWin(tile1, tile2, tile3);
+        }
+    }
+
+    private checkDiagonal1():void {
+        var tile1:Tile = this.getTile(0, 0);
+        var tile2:Tile = this.getTile(1, 1);
+        var tile3:Tile = this.getTile(2, 2);
+
+        this.checkForWin(tile1, tile2, tile3);
+    }
+
+    private checkDiagonal2():void {
+        var tile1:Tile = this.getTile(2, 0);
+        var tile2:Tile = this.getTile(1, 1);
+        var tile3:Tile = this.getTile(0, 2);
+
+        this.checkForWin(tile1, tile2, tile3);
+    }
+
+    private checkForNoWin():void {
+        // can only happen if the game is still ongoing
+        if (this._gameState == GameState.ONGOING) {
+            var allAreOwned:boolean = true;
+
+            // every tile should be clicked by now
+            for (var x=0;x<3;x++) {
+                for (var y=0;y<3;y++) {
+                    var owned:boolean = (this.getTile(x, y).getState() != TileState.Empty);
+
+                    allAreOwned = allAreOwned && owned;
+                }
+            }
+
+            if (allAreOwned) {
+                this._gameState = GameState.WON_BY_NOONE;
+                this.notify();
+            }
+        }
+    }
+
+    private checkGameState() {
+        // check horizontal tiles for a win
+        this.checkHorizontal();
+        
+        // check vertical tiles for a win
+        this.checkVertical();
+
+        // check diagonal tiles for a win
+        this.checkDiagonal1();
+        this.checkDiagonal2();
+
+        // no one won but game is still over
+        this.checkForNoWin();
     }
 
     getTile(x:number, y:number) : Tile {
